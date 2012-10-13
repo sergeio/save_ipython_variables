1dd963b7adacdd6a997a282367e9bac717796720
Sergei Orlov <sorlov+github@gmail.com>
2012-10-13 18:52:50 -0400

    init: Don't use relative import

--- save_ipython_variables/__init__.py
+++ save_ipython_variables/__init__.py
@@ -1,3 +1,6 @@
-from save_variables import load_all_variables, save_variable
+from save_ipython_variables.save_variables import (
+    load_all_variables,
+    save_variable,
+)
 
 __all__ = ['load_all_variables', 'save_variable']
